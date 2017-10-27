63791eef7d35d114906c959562ac11287f6ffc14
unknown <vuvuong8@aol.com>
2017-10-27 10:01:05 -0700

    formatted so no objects in firebase

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -13,15 +13,12 @@ var destina;
 var firstTrain;
 var frequen;
 var timeStamp;
-var current;
 var firstTrainTime;
-var firstTrainMilli;
 var timeDiff;
+var minutesPassedTimer;
 var eta;
-var etaM;
 var arrivalTime;
 var arrival;
-var time;
 $("#add-train").on("click", function(event) {
     event.preventDefault();
     trainName = $('#train-name').val().trim();
@@ -29,31 +26,25 @@ $("#add-train").on("click", function(event) {
     firstTrain = $('#first-train').val().trim();
     frequen = $('#frequency').val().trim();
     timeStamp = firebase.database.ServerValue.TIMESTAMP;
-    current = moment.now();
-    //current time in milliseconds
+    //gets unique timestamp for each iteration
     firstTrainTime = moment(firstTrain, "hh:mm");
-    //converts hh:mm (military time format) string to date
-    firstTrainMilli = Date.parse(firstTrainTime);
-    //converts date attribute into milliseconds
-    timeDiff = current - firstTrainMilli;
-    //gets how many milliseconds have passed since first train
-    minutesDiff = timeDiff / (60 * 1000);
-    //converts elapsed time from milliseconds to minutes
-    eta = minutesDiff % frequen;
-    //retrieves number of minutes till next train IMPORTANT
-    etaM = eta * (60 * 1000);
-    //gets eta converted into milliseconds
-    arrivalTime = current + etaM
-    //adds etaM to current time
-    time = new Date(arrivalTime);
-    //converts arrival time in milliseconds to dollars IMPORTANT
+    //converts hh:mm (military time format) string to date time
+    timeDiff = moment().diff(moment(firstTrainTime), "minutes");
+    //gets how many minutes from first train to present
+    minutesPassedTimer = timeDiff % frequen;
+    //cycles through all trains so far and gives minutes passed in current iterations
+    eta = frequen - minutesPassedTimer;
+    //gets how many minutes until the next train
+    arrivalTime = moment().add(eta, "minutes");
+    //adds eta to current time
+    arrival = moment(arrivalTime).format("hh:mm");
+    //formats cause objects in firebase keeps returning undefined
     dataRef.ref().push({
         train: trainName,
         destination: destina,
         often: frequen,
-        time: time, 
+        arrival: arrival,
         eta: eta
-
     })
 });
 dataRef.ref().on("child_added", function(childSnapshot) {
@@ -62,7 +53,7 @@ dataRef.ref().on("child_added", function(childSnapshot) {
     var tableDataString = "<td>" + childSnapshot.val().train + "</td>" +
         "<td>" + childSnapshot.val().destination + "</td>" +
         "<td>" + childSnapshot.val().often + "</td>" +
-        "<td>" + childSnapshot.val().time + "</td>" +
+        "<td>" + childSnapshot.val().arrival + "</td>" +
         "<td>" + childSnapshot.val().eta + "</td>"
     tableRow.append(tableDataString)
     $("#schedule-table").append(tableRow);
